ae6c735e978f31834dcbf3635c9626aeb5202d1b
rebekahawilson <rebekah2512@gmail.com>
2024-09-05 16:46:27 +0100

    course

---
+++ Introduction To R/Data Analysis in R/Programming_in_R.R
@@ -0,0 +1,18 @@
+# Task 1 - Write a program that performs the operation 130 - 5.
+130 - 5
+
+# Task 2 - Evaluate the following statement and write TRUE or FALSE in the code editor:
+# In R programming, F and FALSE both represent the logical value of "false".
+TRUE
+
+# Task 3 - Consider the scenario where you're studying the boiling point of water, which is typically one hundred degrees Celsius at sea level. How would you represent this temperature as a Numeric literal value in R?
+100.00 
+
+# Task 4 - In a box, there are 125 items. In R language, how would you represent this quantity as an Integer literal?
+125L 
+
+# Task 5a - The profit (selling price minus purchase price) on the sale of one mango using values in the selling price and purchase price columns.
+24.90 - 21.15
+
+# Task 5b - The multiplication operator, in R, is the same as it is in mathematics: x (e.g., 3 x 4). Answer this question with the TRUE or FALSE literal.
+FALSE
